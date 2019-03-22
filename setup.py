c0293549ed9a54d87f1b7906e4f4f1a123e2a417
Marc Enthoven <30950188+MarcEnthoven@users.noreply.github.com>
2019-03-22 16:15:49 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bsn",
+    version="1.0.0",
+    author="Marc Enthoven",
+    author_email="marc_enthoven@hotmail.com",
+    description="This package can be used to test whether a number is a bsn (Burger Service Number, Dutch Social Security Number), but it can also generate the numbers",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/marcenthoven/bsn",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
